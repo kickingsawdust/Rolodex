af9e6b816e913e5849cac97e6e540105d2881912
Bill Loss <madar@kickingsawdust.org>
2023-12-05 15:47:39 -0600

    fixed some button code

--- Prototypes/select_a_card_with_TGB.py
+++ Prototypes/select_a_card_with_TGB.py
@@ -12,14 +12,13 @@ import busio
 import sqlite3
 import subprocess
 import tm1637 #7 segment display
-#import RPi.GPIO as GPIO
 from adafruit_motor import stepper
 from adafruit_motorkit import MotorKit
 from gpiozero import LED, Button
 from time import sleep
 from signal import pause
 
-#Zero Code (remember we need to get a non-locking hall sensor :( )
+# Zero the Carousel
 
 retcode = subprocess.call(["/usr/bin/python", "zero_carousel_smooth.py"])
 if retcode != 0:
@@ -27,7 +26,7 @@ if retcode != 0:
 slot=0
 
 # Set some variables we'll be needing
-#GPIO.setmode(GPIO.BCM)
+
 con = sqlite3.connect("cards.db", check_same_thread=False)
 cur = con.cursor()
 CLK = 15
@@ -35,6 +34,9 @@ DIO = 14
 DELAY = .2
 tm = tm1637.TM1637(clk=CLK, dio=DIO)
 travel = 0
+
+# Display trick name
+
 tm.scroll('Select a card with the Great Billsoni', 100) # 1 fps
 
 #Button variables (what more variables?)
@@ -47,12 +49,9 @@ card_value_button = Button(26)
 card_value_button_count = 0
 card_valueMap = {0:'',1:'A',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9',10:'10',11:'J',12:'Q',13:'K',14:'unset '}
 card_value = "unset"
-#actuator_button = Button(4)
+actuator_button = Button(16)
 actuator_button_state = 0 # this might need to be actuator_button_time stored in seconds?
 selected_card = suit + card_value
-
-#tm.scroll('more crap here', 100) # 1 fps
-
 
 # Button Code
 
@@ -97,68 +96,23 @@ def change_value():
             tm.write([0,0,0,0])
             tm.show(selected_card)
             sleep(DELAY)
-        
-
-
-    #res = cur.execute("SELECT cardVal FROM card2slot WHERE slotVal = ?", (slot,))       
-    #card = res.fetchone()[0]
-    #print("%s" % card)
-    #tm.show(selected_card)
-    #sleep(DELAY)
-    #tm.write([0,0,0,0])
 
 def actuator_motor():
     global actuator_button_state
+    if "unset" not in selected_card:
+        res = cur.execute("SELECT slotVal FROM card2slot WHERE cardVal = ?", (selected_card,))       
+        slot = res.fetchone()[0]
+        print("Actuator button has been pressed moving to slot %s to obtain %s" % (slot, selected_card))
+    else:
+        print("No card selected")
 
 
 suit_button.when_pressed = change_suit
 card_value_button.when_pressed = change_value
-#actuator_button.when_pressed = actuator_motor
+actuator_button.when_pressed = actuator_motor
+
 pause()
 
-
-"""
-
-### NONE OF THIS WORKS PROBABLY COULD JUST REMOVE THIS SECTION COMPLETELY ###
-while True:
-    suit_button_count = 0
-    suit_button.when_pressed()
-    suit_button_count += 1
-    suit = suitMap[suit_button_count]
-    print("The suit is set to: %s" % suit)
-    if suit_button_count > 4:
-        suit_button_count = 0
-        print("Suit has been unset")
-
-while True:
-    card_value_button.wait_for_press()
-    card_value_button_count += 1
-    card_value = card_valueMap[card_value_button_count]
-    print("The value is set to: %s" % card_value)
-    start_time=time.time()
-    if (time.time() - start_time) >=4:
-        selected_card = card_value + suit
-        res = cur.execute("SELECT slotVal FROM card2slot WHERE cardVal = ?", (selected_card,))          
-        slot = res.fetchone()[0]
-        print("Slot is set to %s to obtain card %s" % (slot,selected_card))
-        travel = slot*50
-        count = 0
-        while count < travel:
-            kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
-            count += 1
-            time.sleep(.001)
-
-while True:
-    actuator_button.wait_for_press()
-    actuator__button_count += 1
-    
-    #Figure this crap out, basically if it's pressed actuator engages and pushes the card up. When switch is let go it decends to 0 point
-    #Call to update X card to the selected_card and place the previous X card into the current slot.
-
-
-##########End of garbage button code section################
-
-"""
 
 """
 
